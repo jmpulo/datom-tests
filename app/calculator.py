426abee1aacd2812f63505f9e4ce32aa639b6f84
Chema <jmpujades.dev@proton.me>
2025-01-08 21:19:26 +0100

    added calculator

---
+++ app/calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def add(self, x: int | float, y: int | float) -> int | float:
+        return x + y
+
+    def subtract(self, x: int | float, y: int | float) -> int | float:
+        return x - y
+
+    def multiply(self, x: int | float, y: int | float) -> int | float:
+        return x * y
+
+    def divide(self, x: int | float, y: int | float) -> int | float:
+        if y == 0:
+            raise ValueError("Cannot divide by zero!")
+        return x / y
