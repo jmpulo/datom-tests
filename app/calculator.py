cb6de682c870018a936e8188b041ec773b470bbe
Chema <jmpujades.dev@proton.me>
2025-01-14 18:20:24 +0100

    Add docstring to add method in Calculator class

--- app/calculator.py
+++ app/calculator.py
@@ -1,5 +1,14 @@
 class Calculator:
     def add(self, x: int | float, y: int | float) -> int | float:
+        """Add number to another number
+
+        Args:
+            x (int | float): _description_
+            y (int | float): _description_
+
+        Returns:
+            int | float: _description_
+        """
         return x + y
 
     def subtract(self, x: int | float, y: int | float) -> int | float:
